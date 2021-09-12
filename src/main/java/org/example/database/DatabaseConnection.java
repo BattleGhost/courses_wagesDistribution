0b33e9fd5150ed7c8023cea4c50e1e33e763f2e5
Artur Safonov <assassinsedsed@gmail.com>
2021-09-12 18:04:46 +0300

    Initial commit

---
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -0,0 +1,33 @@
+package org.example.database;
+
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private Connection connection;
+
+    public void createConnection() throws SQLException {
+        Driver driver = new Driver();
+        DriverManager.registerDriver(driver);
+        connection = DriverManager.getConnection(DatabaseConstants.URL, DatabaseConstants.USERNAME,
+                DatabaseConstants.PASSWORD);
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
---
+++ src/main/java/org/example/database/DatabaseConstants.java
@@ -0,0 +1,7 @@
+package org.example.database;
+
+public interface DatabaseConstants {
+    String URL = "jdbc:mysql://localhost:3306/coursesdb";
+    String USERNAME = "root";
+    String PASSWORD = "root";
+}
